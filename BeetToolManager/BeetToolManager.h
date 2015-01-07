ff1bca288ecda3dbc6fc8db8312ff4e5189eece2
leebeet <mac@MactekiMBP.lan>
2015-01-07 23:56:57 +0800

    First version

---
+++ BeetToolManager/BeetToolManager.h
@@ -0,0 +1,13 @@
+//
+//  BeetToolManager.h
+//  BeetToolManager
+//
+//  Created by beet on 1/7/15.
+//  Copyright (c) 2015 Beet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BeetToolManager : NSObject
+
+@end
---
+++ BeetToolManager/BeetToolManager.m
@@ -0,0 +1,13 @@
+//
+//  BeetToolManager.m
+//  BeetToolManager
+//
+//  Created by beet on 1/7/15.
+//  Copyright (c) 2015 Beet. All rights reserved.
+//
+
+#import "BeetToolManager.h"
+
+@implementation BeetToolManager
+
+@end
